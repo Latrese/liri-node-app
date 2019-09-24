d274371a435d96422d61ac8151befc730077d70a
Latrese <latrese.flowers1979@gmail.com>
2019-09-24 11:50:33 -0700

    corrections

--- keys.js
+++ keys.js
@@ -1,9 +1,6 @@
-console.log('this is loaded');
+console.log("Hello. Please access one of the following commands: ");
 
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
-
-
-
--- liri.js
+++ liri.js
@@ -1,21 +1,95 @@
 require("dotenv").config();
 var keys = require("./keys.js");
-// required npm packages
-var Spotify = require('node-spotify-api');
-var axios = require('axios');
-// var db = require('db');
-var moment = require('moment');
+var Spotify = require("node-spotify-api");
+var axios = require("axios");
+var moment = require("moment");
+var fs = require("fs");
+
 var spotify = new Spotify(keys.spotify);
+var argument = process.argv[2];
+var stringArgv = process.argv;
+var userConcert = "";
+var userSong = "";
+var userMovie = "";
 
-// console.log(process.argv);
-
-var userInput = process.argv.splice(2);
-console.log(userInput);
-var liriCommand = userInput[0];
-var searchTerms = userInput.splice(1).map(elem => elem.toLocaleLowerCase()).join('+');
-if (liriCommand === "concert-this"){
-    axios.get("https://rest.bandsintown.com/artists/" + searchTerms + "/events?app_id=codingbootcamp").then(function(response){
-        console.log(response);
-    })
+for (var i = 3; i < stringArgv.length; i++) {
+  if (i > 3 && i < stringArgv.length) {
+    userConcert = userConcert + " " + stringArgv[i];
+    userSong = userSong + " " + stringArgv[i];
+    userMovie = userMovie + "+" + stringArgv[i];
+  } else {
+    userConcert += stringArgv[i];
+    userSong += stringArgv[i];
+    userMovie += stringArgv[i];
+  }
 }
-console.log(searchTerms);
+switch (argument) {
+  case "concert-this":
+    concertThis();
+    break;
+  case "movie-this":
+    movieThis();
+    break;
+  case "hi":
+    Hello();
+    break;
+  case "help":
+    Hello();
+    break;
+  default:
+    console.log(err);
+    break;
+}
+
+function Hello() {
+  console.log("[Concert-This]-[Movie-This]");
+}
+function concertThis() {
+  if (userConcert === "") {
+    userConcert = "ChrisBrown";
+  }
+  var getBiT =
+    "https://rest.bandsintown.com/artists/" +
+    userConcert +
+    "/events?app_id=codingbootcamp";
+  axios.get(getBiT).then(function(response, err) {
+    if (response) {
+      var infoBiT = response.data[0];
+      var venueName = "Venue Name: " + infoBiT.venue.name;
+      var venueLocation = "Venue Location: " + infoBiT.venue.city;
+      var playTime = infoBiT.datetime;
+      var removeTime = playTime.split("T");
+      var venueDate =
+        "Venue Date: " + moment(removeTime[0]).format("MM/DD/YYYY");
+      var venueRecord = venueName + "," + venueLocation + "," + venueDate;
+      console.log(venueName);
+      console.log(venueLocation);
+      console.log(venueDate);
+    } else {
+      console.log(err);
+    }
+  });
+}
+
+function movieThis() {
+  if (userMovie === "") {
+    userMovie = "The+Notebook";
+  }
+  var queryUrl =
+    "http://www.omdbapi.com/?t=" + userMovie + "&y=&plot=short&apikey=trilogy";
+
+  console.log(queryUrl);
+  axios.get(queryUrl).then(function(response, err) {
+    console.log(err ? "Errors occurred: " + err : "");
+    if (response) {
+      console.log("Title: " + response.data.Title);
+      console.log("Release Year: " + response.data.Year);
+      console.log("Rated: " + response.data.Rated);
+      console.log("Actors: " + response.data.Actors);
+      console.log("IMDB Rating: " + response.data.imdbRating);
+      console.log("Description: " + response.data.Plot);
+    } else {
+      console.log(err);
+    }
+  });
+}
